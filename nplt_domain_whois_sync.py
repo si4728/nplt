b327d631b1d71a46c3c25721101615597d7f7a65
si4728 <30460624+si4728@users.noreply.github.com>
2026-06-27 17:30:04 +0900

    Add files via upload

--- nplt_domain_whois_sync.py
+++ nplt_domain_whois_sync.py
@@ -12,7 +12,7 @@ DB_CONFIG = {
     "host": "localhost",
     "port": 3306,
     "user": "root",
-    "password": os.getenv("NPLT_DB_PASSWORD", ""),
+    "password": os.getenv("NPLT_DB_PASSWORD", "P@ssw0rd!@#$"),
     "database": "nplt",
 }
 
